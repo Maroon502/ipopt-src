512b8780a209e7f041d4bbf98ca81ee63713f7a7
Maroon502 <maroon502@outlook.com>
2024-04-11 21:28:06 +0800

    chore(Ipopt): Bump Ipopt from 3.14.14 to 3.14.15

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ use std::path::{Path, PathBuf};
 use coin_build_tools::{coinbuilder, link, utils};
 
 const LIB_NAME: &str = "Ipopt";
-const IPOPT_VERSION: &str = "3.14.14";
+const IPOPT_VERSION: &str = "3.14.15";
 
 fn main() {
     println!(
